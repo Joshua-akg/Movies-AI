d7d279285b7ca0d81c6e1ce56e80fe97a2575a23
Joshua Akingbade <42061848+Joshua-akg@users.noreply.github.com>
2023-02-16 22:00:50 +0000

    added basic season analytics logic

--- analysis.py
+++ analysis.py
@@ -6,14 +6,44 @@ import seaborn as sns
 from sklearn.linear_model import LinearRegression
 
 def main():
-    #set up seaborn
+    #set up seaborn 
     sns.set_theme()
 
     # Read in the Movies dataset
     df = pd.read_csv("movies.csv")
-    df = df.set_index("id")
+    # df = df.set_index("id")
+    df["release_date"] = pd.to_datetime(df["release_date"])
 
-    analyse_budget_revenue(df)
+    #get first row of df
+    # print(df.iloc[0]["release_date"])
+
+    # create new column for season of release
+    df["season"] = df["release_date"].apply(get_season)
+    # print(df["season"])
+
+    # model = LinearRegression()
+    # model.fit(df[["season"]], df["revenue"])
+
+    # X = df[['season']]
+    # y = df['revenue']
+    # y_pred = model.predict(X)
+
+    # plt.scatter(X, y, color='blue')
+    # plt.plot(X, y_pred, color='red', linewidth=2)
+
+    # plt.xlabel('Season of release')
+    # plt.ylabel('Revenue')
+    # plt.show()
+
+def get_season(date):
+    if date.month in (3, 4, 5):
+        return 1
+    elif date.month in (6, 7, 8):
+        return 2
+    elif date.month in (9, 10, 11):
+        return 3
+    else:
+        return 4
 
 def analyse_budget_revenue(df):
     #extract average budget
