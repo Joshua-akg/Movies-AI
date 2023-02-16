2260fc2a500c3e501182bca7ef8494d550b93a6d
Joshua Akingbade <42061848+Joshua-akg@users.noreply.github.com>
2023-02-16 20:04:08 +0000

    plotting predicted trace method

--- analysis.py
+++ analysis.py
@@ -6,18 +6,42 @@ import seaborn as sns
 from sklearn.linear_model import LinearRegression
 
 def main():
+    #set up seaborn
+    sns.set_theme()
+
     # Read in the Movies dataset
     df = pd.read_csv("movies.csv")
     df = df.set_index("id")
 
-    #print column heads
-    print(df.head(),"\n")
+    #extract average budget
+    avg_budget = df["budget"].mean()
+    print("Average budget: ", avg_budget)
 
     #plot budget vs revenue
-    plt.scatter(df["budget"], df["revenue"])
+    sns.scatterplot(x="budget", y="revenue", data=df)
+    # plt.scatter(df["budget"], df["revenue"])
+
     plt.xlabel("Budget")
     plt.ylabel("Revenue")
     plt.title("Budget vs Revenue")
+
+    # Train a linear regression model
+    x = df["budget"].values.reshape(-1, 1)
+    y = df["revenue"].values.reshape(-1, 1)
+
+    model = LinearRegression()
+    model.fit(x, y)
+
+    #for a given budget, predict the revenue
+    revenue = model.predict([[avg_budget]])[0][0]
+    print("Predicted revenue: ", revenue)
+
+    # Plot the regression line
+    plt.plot(x, model.predict(x), color="red")
+
+    # plot a line for the average budget
+    plt.plot([avg_budget, avg_budget], [0, revenue], '--', color="yellow",lw=2)  # dotted line from x-axis to plot
+    plt.plot([0, avg_budget], [revenue, revenue], '--', color = "yellow", lw=2)
 
     plt.show()
 
