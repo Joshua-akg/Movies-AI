2f15328a7ef026b19f29e40e6e63133787eeda7e
Joshua Akingbade <42061848+Joshua-akg@users.noreply.github.com>
2023-02-16 15:43:27 +0000

    import sklearn linear regression

--- analysis.py
+++ analysis.py
@@ -3,14 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from sklearn.linear_model import LinearRegression
+
 def main():
     # Read in the Movies dataset
     df = pd.read_csv("movies.csv")
     df = df.set_index("id")
 
     #print column heads
-    print(df.head())
+    print(df.head(),"\n")
 
+    #plot budget vs revenue
+    plt.scatter(df["budget"], df["revenue"])
+    plt.xlabel("Budget")
+    plt.ylabel("Revenue")
+    plt.title("Budget vs Revenue")
+
+    plt.show()
 
 if __name__ == "__main__":
     main()
