273a9205ea69bbe22dcb1e7f8a5f3edbe6af0fd4
Joshua Akingbade <42061848+Joshua-akg@users.noreply.github.com>
2023-02-16 15:12:26 +0000

    set index to id attribute

--- analysis.py
+++ analysis.py
@@ -6,7 +6,9 @@ import seaborn as sns
 def main():
     # Read in the Movies dataset
     df = pd.read_csv("movies.csv")
+    df = df.set_index("id")
 
+    #print column heads
     print(df.head())
 
 
